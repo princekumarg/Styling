c7bbcd1aa166230277d0bc55da520e4cb52d2625
prince kumar <agarwal8789@gmail.com>
2024-02-26 15:05:07 +0530

    commit made

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navigation from "./Navigation/Navigation"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navigation />
+      <h1>Home123</h1>
+    </>
+  )
+}
+
+export default App
